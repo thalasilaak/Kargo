18cdaafb3dfb533b9d59afd17d3e975211e1b25b
thalasilaak <47997535+thalasilaak@users.noreply.github.com>
2021-04-27 19:07:48 -0400

    Add files via upload
    Edits: 
    - added extra comments to document purpose of each code block
    - changed output method to process.stdout.write to make it a more direct output

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
-
-var inp = process.main.slice(2);
-
-for(var j = 0; j < inp.length;j++ )//iterates through list
+var inp = process.main.slice(2);// parses input and puts it as an int array, slices from 2 to remove the first 2 unnecessary built-in values from input stream
+var output = '';
+for(var k = 0; k < inp.length; k++)//converts all ints within inp array into strings
 {
-    for(var i = 0; i < inp[j].length; i++)//iterates through each word
-    {
-        if(inp[j].charAt(i) === '1')
-        {
-            print('one');
-        } else if(inp[j].charAt(i) === '2') {
-            print('two');
-        } else if(inp[j].charAt(i) === '3'){
-            print('three');
-        } else if (inp[j].charAt(i) === '4') {
-            print('four');
-        } else if (inp[j].charAt(i) === '5') {
-            print('five');
-        } else if (inp[j].charAt(i) === '6') {
-            print('six');
-        } else if (inp[j].charAt(i) === '7') {
-            print('seven');
-        } else if (inp[j].charAt(i) === '8') {
-            print('eight');
-        } else if (inp[j].charAt(i) === '9') {
-            print('nine');
-        } else {
-            print('zero');
-        }
-
-    }
-    print(', ');
+    inp[k] = String(inp[k]);
 }
+for(var i = 0; i < inp.length; i++)//new word starts from input stream
+{
+    for(var j = 0; j < inp[i].length; j++)
+    {
+        if(inp[i].charAt(j) == '1')
+        {
+            output+= 'one';
+        } else if(inp[i].charAt(j) == '2')
+        {
+            output += 'two';
+        } else if(inp[i].charAt(j) == '3')
+        {
+            output += 'three';
+        } else if(inp[i].charAt(j) == '4')
+        {
+            output += 'four';
+        } else if(inp[i].charAt(j) == '5')
+        {
+            output += 'five';
+        } else if(inp[i].charAt(j) == '6')
+        {
+            output += 'six';
+        } else if(inp[i].charAt(j) == '7')
+        {
+            output+= 'seven';
+        } else if(inp[i].charAt(j) == '8')
+        {
+            output += 'eight';
+        } else if(inp[i].charAt(j) == '9')
+        {
+            output += 'nine';
+        } else {
+            output += 'zero';
+        }
+    }//word ends
+    output += ', ';
+}
+process.stdout.write(output);//changed to directly output instead of using different method
